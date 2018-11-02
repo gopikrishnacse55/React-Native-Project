c9409612b2c4db91e37614c78dc65f9b2ef659a4
Gopi Krishna <gopikrishna@Admins-Mac-mini.local>
2018-11-02 14:41:49 +0530

    Fetch Data from Server

--- ios/Components/Dashboard.js
+++ ios/Components/Dashboard.js
@@ -7,33 +7,107 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View,TextInput,TouchableOpacity} from 'react-native';
+import {Platform, StyleSheet, Text, View,TextInput,FlatList,Image,ActivityIndicator} from 'react-native';
+import APIData from '../Components/APIData'
 export default class Dashboard extends Component 
 {
 
-  
-sendDatatoFrontView = () =>
-{
-  this.props.navigation.goBack();
+  constructor(props)
+  {
+    super(props)
+    this.state = {
+      dealsArray : [],
+      isLoadingIndicator : true,
+    }
+  }
+
+
+FlatListItemSeparator = () =>
+ {
+    return(
+      <View style = {styles.separatorStyle}>
+      </View>
+    )
+    
 }
 
+async componentDidMount()
+{
+  const deals = await APIData.fetchDataFromAPI();
+  this.setState({
+    dealsArray : deals,
+    isLoadingIndicator : false
+  })
+}
 
-  render() {
-    const {params} = this.props.navigation.state;
-    return (
-      <View style = {styles.container}>
-    </View>
-    );
+  render() 
+  {
+
+      if (this.state.isLoadingIndicator)
+      {
+        return (
+          <View style={styles.indicatorStyle}>
+              <ActivityIndicator  size="large" color="black"/>
+          </View>
+        )
+      }
+      else
+      {
+        return(
+
+          <View style = {styles.container}>
+          <FlatList
+                   data={this.state.dealsArray}
+                   ItemSeparatorComponent = {this.FlatListItemSeparator}
+                   renderItem={({item}) => <View style = {styles.container}><Text  style = {styles.welcome}>Title : {item.title}</Text>
+                   <Text  style = {styles.priceStyle}>Price : {item.price}</Text>
+                   <Image style = {styles.imageStyle} source={{uri: item.media[0]}}/>
+                   </View>
+                 }
+               />
+        </View>
+        )
+      }
+
   }
 }
 
 
 const styles = StyleSheet.create ({
-   container: {
-     flex : 1,
-         flexDirection: 'column',
-         justifyContent:'center',
-         alignItems : 'stretch',
-        //  backgroundColor : 'grey',
-   },
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    paddingBottom : 5
+    },
+    welcome: {
+      fontSize: 15,
+      color:'black',
+      padding:15,
+      paddingBottom : 5
+      },
+   priceStyle :
+      {
+      fontSize: 13,
+      color:'black',
+      marginHorizontal : 20,
+      paddingBottom : 10
+      },
+    imageStyle : {
+        width : 60,
+        height : 60,
+         marginHorizontal : 20,
+        paddingTop : 5,
+    },
+    indicatorStyle: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    separatorStyle :
+    {
+      height: 1,
+      width: "100%",
+      backgroundColor: "#808080",
+    }
 })
